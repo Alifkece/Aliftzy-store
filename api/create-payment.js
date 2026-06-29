0d3ab9e8f0e5abda0237cec848cb652bcd473de0
AliftzyOffc <alifmuhammadalifudin@gmail.com>
2026-06-29 23:51:24 +0700

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,57 +1,76 @@
 export default async function handler(req, res) {
 
-  if (req.method !== "POST") {
-    return res.status(405).json({
-      message: "Method not allowed"
-    });
-  }
+if (req.method !== "POST") {
+return res.status(405).json({
+message: "Method not allowed"
+});
+}
 
-  try {
+try {
 
-    const {
-      amount,
-      username
-    } = req.body || {};
+let raw = "";  
 
+await new Promise((resolve) => {  
 
-    if (!amount || !username) {
-      return res.status(400).json({
-        error: "amount dan username wajib diisi",
-        body: req.body
-      });
-    }
+  req.on("data", chunk => {  
+    raw += chunk;  
+  });  
 
+  req.on("end", resolve);  
 
-    const response = await fetch(
-      "https://rest.sitranfer.com/payment/api/generate",
-      {
-        method: "POST",
-
-        headers: {
-          "Content-Type": "application/json"
-        },
-
-        body: JSON.stringify({
-          key: process.env.SITRANSFER_KEY,
-          channel: "QRIS",
-          amount: Number(amount),
-          player_username: username
-        })
-      }
-    );
+});  
 
 
-    const result = await response.json();
-
-    return res.status(200).json(result);
+const body = JSON.parse(raw);  
 
 
-  } catch (err) {
+const amount = body.amount;  
+const username = body.username;  
 
-    return res.status(500).json({
-      error: err.message
-    });
 
-  }
+if (!amount || !username) {  
+  return res.status(400).json({  
+    error:"amount dan username wajib diisi",  
+    raw  
+  });  
+}  
+
+
+const response = await fetch(  
+  "https://rest.sitranfer.com/payment/api/generate",  
+  {  
+    method:"POST",  
+
+    headers:{  
+      "Content-Type":"application/json"  
+    },  
+
+    body:JSON.stringify({  
+
+    key: process.env.SITRANSFER_KEY,  
+
+      channel:"QRIS",  
+
+      amount:Number(amount),  
+
+      player_username:username  
+
+    })  
+  }  
+);  
+
+
+const result = await response.json();  
+
+
+return res.status(200).json(result);
+
+} catch(err) {
+
+return res.status(500).json({  
+  error:err.message  
+});
+
+}
 
 }
