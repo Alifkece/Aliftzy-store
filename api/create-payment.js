bb1ca3981ff9882973f6d262160b298e49ca574e
AliftzyOffc <alifmuhammadalifudin@gmail.com>
2026-06-29 15:20:02 +0700

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -9,20 +9,30 @@ export default async function handler(req, res) {
 
   try {
 
-    let body = req.body;
+    let raw = "";
 
-    if (typeof body === "string") {
-      body = JSON.parse(body);
-    }
+    await new Promise((resolve) => {
+
+      req.on("data", chunk => {
+        raw += chunk;
+      });
+
+      req.on("end", resolve);
+
+    });
 
 
-    const { amount, username } = body || {};
+    const body = JSON.parse(raw);
+
+
+    const amount = body.amount;
+    const username = body.username;
 
 
     if (!amount || !username) {
       return res.status(400).json({
-        error: "amount dan username wajib diisi",
-        received: body
+        error:"amount dan username wajib diisi",
+        raw
       });
     }
 
@@ -31,14 +41,21 @@ export default async function handler(req, res) {
       "https://rest.sitranfer.com/payment/api/generate",
       {
         method:"POST",
+
         headers:{
           "Content-Type":"application/json"
         },
+
         body:JSON.stringify({
+
           key: process.env.SITRANSFER_KEY,
+
           channel:"QRIS",
+
           amount:Number(amount),
+
           player_username:username
+
         })
       }
     );
@@ -50,11 +67,13 @@ export default async function handler(req, res) {
     return res.status(200).json(result);
 
 
-  } catch(error){
+  } catch(err) {
+
 
     return res.status(500).json({
-      error:error.message
+      error:err.message
     });
+
 
   }
 
